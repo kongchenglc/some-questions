49d7f6ee6ce441acbc408fadabdb244828f67225
kongchenglc@gmail.com <kongchenglc@gmail.com>
2017-09-04 17:01:25 +0800

    9.4change

--- function.valueof=.js
+++ function.valueof=.js
@@ -1,7 +1,6 @@
 function mul(x) {
 	const result = (y) => mul(x * y); 
 	//valueOf函数重写，在求值的时候会调用
-	result.valueOf();
 	result.valueOf = () => {console.log("diaoyongle"); return x};
 	return result;
 }
