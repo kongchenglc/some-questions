a6e4e86c192c4d5e1e88170fd4cc34239add4572
kongchenglc <kongchenglc@gmail.com>
2018-09-11 16:24:33 +0800

    update

--- Interview questions/IO牛客网.js
+++ Interview questions/IO牛客网.js
@@ -1,13 +1,15 @@
-const input = `
-2
-0 0
-0 3
-`
-const lines = input.split('\n').filter(item => item)
-let Hindex = 0;
 function readline() {
-	return lines[Hindex++]
+	return readline.lines[readline.Hindex++]
 }
+readline.input = `
+4
+3 1 2 3
+4 1 1 1 3
+2 2 3
+2 3 4
+`
+readline.lines = readline.input.split('\n').filter(item => item)
+readline.Hindex = 0;
 function print(msg) {
 	console.log(msg)
 }
